0977b8d639a97f16f3e5b7d5ade390791c1f3bfd
Hossein <mh.amiri.notash@gmail.com>
2025-07-02 19:50:32 +0330

    feat(frontend) : Adds initial API endpoint configuration.

---
+++ Frontend/api/endpoints.ts
@@ -0,0 +1,12 @@
+const API_BASE_URL = "http://127.0.0.1:8000/api/";
+
+const endpoints = {
+    search : "Employees/search",
+    getAllEmployees_AndStore : "Employees",
+    getOneEmpById_UpdateAndDeleteEMP : "Employees/{id}",
+    eportExcel : "export-employees"
+};
+
+export const getApiUrl = (key : keyof typeof endpoints) => {
+    return `${API_BASE_URL}${endpoints[key]}`;
+};
